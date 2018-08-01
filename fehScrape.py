2ff246747e76517c690c27667cb568c4c092ba47
Michael Au-Duong <mauduong723@gmail.com>
2018-08-01 20:39:26 +1200

    Update: SP, RNG, Might, Turns

--- fehScrape.py
+++ fehScrape.py
@@ -6,7 +6,7 @@ from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
-from HTMLParser import HTMLParseError
+from html.parser import HTMLParser
 import csv
 import requests
 import re
@@ -49,17 +49,23 @@ def scrapeURL(soup, url):
 # Get a single Hero's name and title
 def getName(soup):
 	try: 
+		#origin = soup.find('div', attrs={'class': 'field__item'}).get_text().strip()
 		for name in soup.find('table', attrs={'id': 'hero-details-table'}).find('span'):
 			title = name.findNext('span').get_text().replace('-', '').strip()
 			
 		dataArray.append(name)
 		dataArray.append(title)
+		#dataArray.append(origin)
+		dataArray.append("\n")
 	except Exception as e:
 		log_error("Error during name and title scraping {0} : {1}\n".format(soup, str(e)))
 
 # Function to gather NEUTRAL IV stats of the specified Hero
 def getStats(soup):
 	try:
+		totalBST = soup.find('span', attrs={'class': 'max-stats-number'}).get_text().strip()
+		totalBST = "Base Stats Total: " + totalBST
+		dataArray.append(totalBST)
 		for headerStats in soup.find_all('div', attrs={'class': 'header-stats'}):
 			head = headerStats.get_text().strip()
 			dataArray.append(head)
@@ -71,31 +77,40 @@ def getSkills(soup):
 		# Get all available weapons of the specified Hero
 		for weapon in soup.find_all('td', attrs={'headers': 'view-title-table-column', 'class': 
 		'views-field views-field-title views-field-field-weapon-effect views-field-field-star-defaults views-field-field-stars views-field-description__value'}):
+			weaponSP = weapon.findNext('td', {'headers': 'view-field-weapon-skills-sp-cost-table-column'}).get_text()
+			weaponRNG = weapon.findNext('td', {'headers': 'view-field-weapon-range-table-column'}).get_text()
+			weaponMT = weapon.findNext('td', {'headers': 'view-field-weapon-power-table-column'}).get_text()
 			weaponName = weapon.get_text().strip()
-			weaponDesc = "WEAPON: " + weaponName
+			weaponDesc = "WEAPON: " + weaponName + " SP: " + weaponSP + " RNG: " + weaponRNG + " MIGHT: " + weaponMT
 			dataArray.append(weaponDesc)
-			
+		
 		# Get all available support skills
 		for assist in soup.find_all('td', attrs={'headers': 'view-title-table-column--3', 'class': 
 		'views-field views-field-title views-field-field-command-skill-effect views-field-field-star-defaults views-field-field-stars'}):
+			assistRNG = assist.findNext('td', {'headers': 'view-field-command-skill-range-table-column'}).get_text()
+			assistSP = assist.findNext('td', {'headers': 'view-field-command-skills-sp-requirem-table-column'}).get_text()
 			assistName = assist.get_text().strip()
-			assistDesc = "SUPPORT: " + assistName
+			assistDesc = "SUPPORT: " + assistName + " RNG: " + assistRNG + " SP: " + assistSP
 			dataArray.append(assistDesc)
-			
+		
 		# Get all available specials of the specified Hero
 		for special in soup.find_all('td', attrs={'headers': 'view-title-table-column--3', 'class':
 		'views-field views-field-title views-field-field-special-skill-effect views-field-field-star-defaults views-field-field-stars views-field-description__value'}):
+			specialSP = special.findNext('td', {'headers': 'view-field-special-skills-sp-cost-table-column'}).get_text()
+			specialTurns = special.findNext('td', {'headers': 'view-field-special-skill-turn-require-table-column'}).get_text()
 			specialName = special.get_text().strip()
-			specialDesc = "SPECIAL: " + specialName
+			specialDesc = "SPECIAL: " + specialName + " SP: " + specialSP + " TURNS: " + specialTurns
 			dataArray.append(specialDesc)
 		
 		# Get all available passive A, B, C skills of the specified Hero
 		for passive in soup.find_all('td', attrs={'headers': 'view-field-passive-skill-icon-table-column', 'class': 
 		'views-field views-field-field-passive-skill-icon views-field-title views-field-field-passive-skills-effect views-field-field-stars views-field-description__value'}):
+			passiveSP = passive.findNext('td', {'headers': 'view-field-passive-skills-sp-cost-table-column'}).get_text()
+			passiveSlot = passive.findNext('td', {'headers': 'view-field-passive-slot-table-column'}).get_text()
 			passiveName = passive.get_text().strip()
-			passiveDesc = passiveName
+			passiveDesc = passiveSlot + " SKILL: " + passiveName + " SP: " + passiveSP 
 			dataArray.append(passiveDesc)
-			
+		
 	except Exception as e:
 		log_error("Error during Skills scraping {0} : {1}\n".format(soup, str(e)))
 
